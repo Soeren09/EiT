b51001c9efb9651611f3e01c3b16ddbdec4dab13
Linux alln <alln@universal-robots.com>
2020-12-09 11:53:39 +0100

    updated AbsolutePaths.h, and tried to get gitignore to work as intended.

--- pluginUIapp/src/AbsolutePaths.h
+++ pluginUIapp/src/AbsolutePaths.h
@@ -2,7 +2,12 @@
 #define PLUGINUIAPP_ABSOLUTEPATHS_H
 #include <string>
 
-std::string URFilePath = "/home/maltenj/EiT/Workcell/Scene.wc.xml";
-std::string GantryFilePath = "/home/maltenj/EiT/Workcell/wsg50/wsg50.xml";
+//Malte:
+// std::string URFilePath = "/home/maltenj/EiT/Workcell/Scene.wc.xml";
+// std::string GantryFilePath = "/home/maltenj/EiT/Workcell/wsg50/wsg50.xml";
+
+//Alexander
+std::string URFilePath = "/home/alexander/EiT/Practical/EiT/Workcell/Scene.wc.xml";
+std::string GantryFilePath = "/home/alexander/EiT/Practical/EiT/Device_Tryouts/Total.xml";
 
 #endif
